cd54ffef539502652c466f2cccc95cd2b41e8aed
Antonio Brewer <yodabox64@hotmail.com>
2023-03-03 03:50:11 -0500

    Add files via upload

--- update.bat
+++ update.bat
@@ -2,4 +2,5 @@ if not DEFINED IS_MINIMIZED set IS_MINIMIZED=1 && start "" /min "%~dpnx0" %* && 
 TASKKILL /IM HMI-Control.exe /F
 if not exist %USERPROFILE%\Desktop\HMIControl (mkdir %USERPROFILE%\Desktop\HMIControl)
 curl -Lo %USERPROFILE%\Desktop\HMIControl\HMI-Control.exe https://github.com/tonton81/oruprotal/blob/main/HMI-Control.exe?raw=true
+start "" "%USERPROFILE%\Desktop\HMIControl\HMI-Control.exe"
 exit
